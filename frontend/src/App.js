f603ec27860a6427c0c4d68bbade033e78647424
Shubham Singh <shubhamkusingh7000@gmail.com>
2025-10-31 12:48:44 +0530

    Initial project setup: frontend + backend structure

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="app">
+      <main className="container">
+        <h1>Detagenix</h1>
+        <p>Welcome to the Detagenix website!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
+    
